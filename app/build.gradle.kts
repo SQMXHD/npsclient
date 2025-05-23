6be21ea625f34988f981da41b7ee6b754ecb5402
djylb <duan@d-jy.net>
2025-05-23 20:00:50 +0000

    Update NPC to 0.29.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 import java.util.Properties
 
-val appVersionCode = 29
-val appVersionName = "1.0.27"
-val npsVersion = "0.29.6"
+val appVersionCode = 30
+val appVersionName = "1.0.28"
+val npsVersion = "0.29.7"
 val npcFileName = "libnpc.so"
 
 plugins {
